c57032d41a47e3007f5884869022fb909f5c756a
Rick <rmartinez1611@gmail.com>
2021-03-18 16:38:55 -0400

    just saving progress, going to move to chat features now

--- Slacker Clone/Controller/LogInViewController.swift
+++ Slacker Clone/Controller/LogInViewController.swift
@@ -10,27 +10,33 @@ import UIKit
 class LogInViewController: UIViewController {
     
     @IBOutlet weak var icon: UIImageView!
-    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var logInButton: UIButton!
     @IBOutlet weak var registerButton: UIButton!
     
+    let icon1 = UIImage(systemName: "person.crop.circle.badge.questionmark")
+    let icon2 = UIImage(systemName: "person.crop.circle.badge.checkmark")
+    let icon3 = UIImage(systemName: "person.crop.circle.badge.person.crop.circle.badge.xmark")
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        userNameTextField.delegate = self
+        emailTextField.delegate = self
         passwordTextField.delegate = self
         
     }
+    
     @IBAction func logInButtonTapped(_ sender: UIButton) {
-        NetworkManager.instance.loginUser(email: userNameTextField.text!, password: passwordTextField.text!) { (success) in
+        NetworkManager.instance.loginUser(email: emailTextField.text!, password: passwordTextField.text!) { [self] (success) in
             if success {
+                
                 self.performSegue(withIdentifier: "afterLogInSegue", sender: nil)
-                print(NetworkManager.instance.authToken)
-                print(NetworkManager.instance.userEmail)
+                
+            } else {
+                
             }
         }
-        
     }
     
     
