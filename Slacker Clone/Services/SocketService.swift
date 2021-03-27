73eea85761f935946dc8d77fc64b10b3b838de48
Rick <rmartinez1611@gmail.com>
2021-03-27 10:47:46 -0400

    trying different ways of connecting socket

--- Slacker Clone/Services/SocketService.swift
+++ Slacker Clone/Services/SocketService.swift
@@ -8,51 +8,32 @@
 import Foundation
 import SocketIO
 
-let manager = SocketManager(socketURL: URL(string:"\(NetworkManager.instance.BASE_URL)")!)
-let defaultNamespaceSocket = manager.defaultSocket
-let swiftSocket = manager.socket(forNamespace: "/swift")
+let manager = SocketManager(socketURL: URL(string:"\(NetworkManager.instance.BASE_URL)")!, config: [.log(true), .compress])
+var socket = manager.defaultSocket
 
 
     
-class SocketService: NSObject {
+class SocketService {
     
     static let instance = SocketService()
     
-    override init() {
-        super.init()
-    }
-    
+   
     func establishConnection() {
-//        socket.on("newMessage") {data, ack in
-//            print("socket connected")
-//        }
-//
-//        socket.on("newMessage") {data, ack in
-//            guard let cur = data[0] as? Double else { return }
-//
-//            socket.emitWithAck("canUpdate", cur).timingOut(after: 0) {data in
-//                if data.first as? String ?? "passed" == SocketAckStatus.noAck {
-//                    // Handle ack timeout
-//                }
-//
-//                socket.emit("update", ["amount": cur + 2.50])
-//            }
-//
-//            ack.with("Got your currentAmount", "dude")
-//        }
+        
+        socket.connect()
 
-        swiftSocket.connect()
+            
     }
     
     func closeConnection() {
-        swiftSocket.disconnect()
+        socket.disconnect()
     }
     
     func addMessage(messageBody: String, userId: String, channelId: String, completion: @escaping (Bool) -> Void ) {
         
         let user = NetworkManager.instance.loggedInUser!
         
-        swiftSocket.emit("newMessage", messageBody, userId, channelId, user.name, user.avatarName, user.avatarColor )
+        socket.emit("newMessage", messageBody, userId, channelId, user.name, user.avatarName, user.avatarColor )
         
         completion(true)
     }
