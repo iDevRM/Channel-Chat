e814c76db96d9b7e76de06366efb714c84d78f1a
Rick <rmartinez1611@gmail.com>
2021-03-29 14:35:42 -0400

    working on socket connection

--- Slacker Clone/Services/SocketService.swift
+++ Slacker Clone/Services/SocketService.swift
@@ -8,33 +8,33 @@
 import Foundation
 import SocketIO
 
-let manager = SocketManager(socketURL: URL(string:"\(NetworkManager.instance.BASE_URL)")!, config: [.log(true), .compress])
-var socket = manager.defaultSocket
+
 
 
     
-class SocketService {
+class SocketService: NSObject {
+    
+    var manager = SocketManager(socketURL: URL(string: "\(NetworkManager.instance.BASE_URL)")!, config: [.log(true), .compress])
     
     static let instance = SocketService()
     
-   
-    func establishConnection() {
-        
+    override init() {
+        super.init()
+        let socket = manager.defaultSocket
+        socket.on(clientEvent: .connect) { (data, ack) in
+            print("Socket Connected")
+        }
         socket.connect()
-
-            
     }
     
-    func closeConnection() {
-        socket.disconnect()
-    }
+   
     
     func addMessage(messageBody: String, userId: String, channelId: String, completion: @escaping (Bool) -> Void ) {
-        
+
         let user = NetworkManager.instance.loggedInUser!
-        
+        let socket = manager.defaultSocket
         socket.emit("newMessage", messageBody, userId, channelId, user.name, user.avatarName, user.avatarColor )
-        
+
         completion(true)
     }
     
