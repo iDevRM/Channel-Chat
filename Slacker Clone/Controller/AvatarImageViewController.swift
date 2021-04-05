8392619efdb3cfd3dbcf2e29c36aa4725790fef7
Rick <rmartinez1611@gmail.com>
2021-04-05 16:13:37 -0400

    working on changing avatar for current user

--- Slacker Clone/Controller/AvatarImageViewController.swift
+++ Slacker Clone/Controller/AvatarImageViewController.swift
@@ -14,9 +14,10 @@ class AvatarImageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
+        view.backgroundColor = #colorLiteral(red: 0.7346229553, green: 0.7302576303, blue: 0.7379794717, alpha: 1)
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.layer.cornerRadius = 10
         collectionView.backgroundColor = .white
         
     }
@@ -35,16 +36,21 @@ extension AvatarImageViewController: UICollectionViewDelegate, UICollectionViewD
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AvatarCell", for: indexPath) as? AvatarImageCell {
-            if segmentControl.selectedSegmentIndex == 0 {
-                cell.configCell(imageName: "light\(indexPath.row)")
-                return cell
-            } else {
-                cell.configCell(imageName: "dark\(indexPath.row)")
-                return cell
-            }
-            
+            cell.configCell(imageNumber: indexPath.row, colorSelected: segmentControl.selectedSegmentIndex)
+            return cell
         }
         return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        if segmentControl.selectedSegmentIndex == 0 {
+            NetworkManager.instance.loggedInUser?.avatarName = "light\(indexPath.row)"
+        } else {
+            NetworkManager.instance.loggedInUser?.avatarName = "dark\(indexPath.row)"
+        }
+        dismiss(animated: true, completion: nil)
+        
     }
     
     
--- Slacker Clone/View/AvatarImageCell.swift
+++ Slacker Clone/View/AvatarImageCell.swift
@@ -11,17 +11,29 @@ class AvatarImageCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
     
-    func configCell(imageName: String) {
-        setupView()
-        imageView.image = UIImage(named: imageName)
+    func configCell(imageNumber: Int, colorSelected: Int) {
+        setupView(from: colorSelected)
+        if colorSelected == 0 {
+            imageView.image =  UIImage(named: "light\(imageNumber)")
+            } else {
+                imageView.image = UIImage(named: "dark\(imageNumber)")
+            }
+        
         
     }
     
-    func setupView() {
-        imageView.backgroundColor = UIColor.lightGray
-        imageView.layer.cornerRadius = 10
-        imageView.layer.borderWidth = 2
-        imageView.layer.borderColor = UIColor.darkGray.cgColor
+    func setupView(from colorSelected: Int) {
+        if colorSelected == 0 {
+            imageView.backgroundColor = UIColor.gray
+            imageView.layer.cornerRadius = 10
+            imageView.layer.borderWidth = 2
+            imageView.layer.borderColor = UIColor.darkGray.cgColor
+        } else {
+            imageView.backgroundColor = UIColor.lightGray
+            imageView.layer.cornerRadius = 10
+            imageView.layer.borderWidth = 2
+            imageView.layer.borderColor = UIColor.darkGray.cgColor
+        }
     }
     
 }
